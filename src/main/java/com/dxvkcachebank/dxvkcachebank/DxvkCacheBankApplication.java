7203dd3063da7da7bce1b004f30883628aef48ae
HereticWay <vpleft@gmail.com>
2022-05-25 12:45:31 +0200

    add initial project files;

---
+++ src/main/java/com/dxvkcachebank/dxvkcachebank/DxvkCacheBankApplication.java
@@ -0,0 +1,13 @@
+package com.dxvkcachebank.dxvkcachebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxvkCacheBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxvkCacheBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dxvkcachebank/dxvkcachebank/DxvkCacheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxvkcachebank.dxvkcachebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxvkCacheBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
