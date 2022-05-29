7c8e14b64d9bbad33fdaf8ee241d7f95d6e9ae9b
HereticWay <vpleft@gmail.com>
2022-05-29 23:17:08 +0200

    improve CacheFile entity;

--- src/main/java/com/dxvkcachebank/dxvkcachebank/entity/CacheFile.java
+++ src/main/java/com/dxvkcachebank/dxvkcachebank/entity/CacheFile.java
@@ -1,11 +1,9 @@
 package com.dxvkcachebank.dxvkcachebank.entity;
 
 import lombok.Data;
+import org.springframework.lang.Nullable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,11 +13,28 @@ public class CacheFile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(
+            name = "upload_date",
+            nullable = false
+    )
     private LocalDateTime uploadDateTime;
 
+    @ManyToOne
+    @JoinColumn(
+            foreignKey =
+                @ForeignKey(name = "uploader_id"),
+            nullable = false
+    )
     private User uploader;
 
+    @ManyToOne
+    @JoinColumn(
+            foreignKey =
+                @ForeignKey(name = "game_id"),
+            nullable = false
+    )
     private Game game;
 
+    @Lob
     private byte[] data;
 }
