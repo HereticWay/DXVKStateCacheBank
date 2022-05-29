fc2066ab9b5193a41c3fdd4bf69ddc475093dfa3
HereticWay <vpleft@gmail.com>
2022-05-29 23:17:19 +0200

    improve User entity;

--- src/main/java/com/dxvkcachebank/dxvkcachebank/entity/User.java
+++ src/main/java/com/dxvkcachebank/dxvkcachebank/entity/User.java
@@ -2,26 +2,32 @@ package com.dxvkcachebank.dxvkcachebank.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Data
+@Table(name = "`user`")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String secretKey;
-
+    @Column(
+            nullable = false,
+            unique = true
+    )
     private String name;
 
+    @Column(
+            nullable = false,
+            unique = true
+    )
     private String email;
 
+    @Lob
     private byte[] profilePicture;
 
+    @OneToMany(mappedBy = "uploader")
     private List<CacheFile> contributions;
 }
