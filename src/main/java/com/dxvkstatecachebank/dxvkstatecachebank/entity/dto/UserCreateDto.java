4a911053aa72ba66532c331a64eacdb022473c70
HereticWay <vpleft@gmail.com>
2022-06-12 00:05:46 +0200

    add UserCreateDto;

---
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/dto/UserCreateDto.java
@@ -0,0 +1,17 @@
+package com.dxvkstatecachebank.dxvkstatecachebank.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserCreateDto {
+    private String name;
+    private String password;
+    private String email;
+    private byte[] profilePicture;
+}
