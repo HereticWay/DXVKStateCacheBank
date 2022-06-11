69b9f955434e87be559c9648777da964ce0b71b4
HereticWay <vpleft@gmail.com>
2022-06-12 00:52:21 +0200

    rename CacheFileInfoMapper to CacheFileMapper; improve CacheFileMapper;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/mapper/CacheFileMapper.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/mapper/CacheFileMapper.java
@@ -1,11 +1,15 @@
 package com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper;
 
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.CacheFile;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.Game;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.User;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.CacheFileInfoDto;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.CacheFileUploadDto;
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CacheFileInfoMapper {
+public class CacheFileMapper {
     public CacheFileInfoDto toDto(CacheFile cacheFile) {
         Long uploaderId = cacheFile.getUploader().getId();
         Long gameId = cacheFile.getGame().getId();
@@ -16,6 +20,17 @@ public class CacheFileInfoMapper {
                 .uploaderLink("/user/%d".formatted(uploaderId))
                 .gameLink("/game/%d".formatted(gameId))
                 .dataLink("/cachefile/%d/data")
+                .build();
+    }
+
+    public CacheFile toCacheFile(CacheFileUploadDto cacheFileUploadDto) {
+        Long uploaderId = cacheFileUploadDto.getUploaderId();
+        Long gameId = cacheFileUploadDto.getGameId();
+
+        return CacheFile.builder()
+                .uploader(User.builder().id(uploaderId).build())
+                .game(Game.builder().id(gameId).build())
+                .data(BlobProxy.generateProxy(cacheFileUploadDto.getData()))
                 .build();
     }
 }
