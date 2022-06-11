7a6446b7781074b9f6be3c522e7c0eb9c06a71bd
HereticWay <vpleft@gmail.com>
2022-06-12 00:20:59 +0200

    rename UserInfoMapper.java to UserMapper; improve UserMapper;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/mapper/UserMapper.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/mapper/UserMapper.java
@@ -1,11 +1,12 @@
 package com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper;
 
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.User;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.UserCreateDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.UserInfoDto;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserInfoMapper {
+public class UserMapper {
     public UserInfoDto toDto(User user) {
         return UserInfoDto.builder()
                 .id(user.getId())
@@ -13,6 +14,14 @@ public class UserInfoMapper {
                 .email(user.getEmail())
                 .profilePictureLink("/user/%d/profile_picture".formatted(user.getId()))
                 .contributionsLink("/cachefile/user/%d".formatted(user.getId()))
+                .build();
+    }
+
+    public User toUser(UserCreateDto userCreateDto) {
+        return User.builder()
+                .email(userCreateDto.getEmail())
+                .name(userCreateDto.getName())
+                .password(userCreateDto.getPassword())
                 .build();
     }
 }
