aa3b5029c730e6732b2a13291847046704dc5fea
HereticWay <vpleft@gmail.com>
2022-06-12 00:35:41 +0200

    rename GameInfoMapper to GameMapper; improve GameMapper;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/mapper/GameMapper.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/mapper/GameMapper.java
@@ -1,11 +1,12 @@
 package com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper;
 
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.Game;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.GameCreateDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.GameInfoDto;
 import org.springframework.stereotype.Component;
 
 @Component
-public class GameInfoMapper {
+public class GameMapper {
     public GameInfoDto toDto(Game game) {
         Long incrementalCacheFileId = game.getLatestCacheFile().getId();
         return GameInfoDto.builder()
@@ -14,6 +15,13 @@ public class GameInfoMapper {
                 .incrementalCacheFileLink("/cachefile/%d/data".formatted(incrementalCacheFileId))
                 .contributionsLink("/cachefile/game/%d".formatted(game.getId()))
                 .steamId(game.getSteamId())
+                .build();
+    }
+
+    public Game toGame(GameCreateDto gameCreateDto) {
+        return Game.builder()
+                .name(gameCreateDto.getName())
+                .steamId(gameCreateDto.getSteamId())
                 .build();
     }
 }
