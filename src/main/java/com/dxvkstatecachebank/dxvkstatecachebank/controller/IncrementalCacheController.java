526a6ad9eaa3d7435dbc741328a207a029e646b5
HereticWay <vpleft@gmail.com>
2022-06-12 23:02:18 +0200

    rename mapping of IncrementalCacheController to /incrcachefile;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/IncrementalCacheController.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/IncrementalCacheController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/incrcachefile")
 public class IncrementalCacheController {
     @Autowired
     private IncrementalCacheService incrementalCacheService;
