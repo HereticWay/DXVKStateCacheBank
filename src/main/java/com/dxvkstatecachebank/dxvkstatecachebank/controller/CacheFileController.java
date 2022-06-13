019097bb6a1dc2e3922aa404378d9c7497f4458c
HereticWay <vpleft@gmail.com>
2022-06-13 13:59:55 +0200

    optimize CacheController;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/CacheFileController.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/CacheFileController.java
@@ -1,18 +1,22 @@
 package com.dxvkstatecachebank.dxvkstatecachebank.controller;
 
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.CacheFile;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.Game;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.IncrementalCacheFile;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.CacheFileInfoDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.CacheFileUploadDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper.CacheFileMapper;
 import com.dxvkstatecachebank.dxvkstatecachebank.service.CacheFileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
-import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -39,31 +43,26 @@ public class CacheFileController {
 
     @Transactional
     @GetMapping("/{cacheFileId}/data")
-    public ResponseEntity<StreamingResponseBody> getCacheFileData(@PathVariable("cacheFileId") Long cacheFileId, final HttpServletResponse response) {
+    public ResponseEntity<Resource> getCacheFileData(@PathVariable("cacheFileId") Long cacheFileId) throws SQLException {
         CacheFile cacheFile = cacheFileService.findById(cacheFileId);
-        String cacheFileName = cacheFile.getGame()
-                .getCacheFileName();
+        Blob cacheFileBlob = cacheFile.getData();
+        Game game = cacheFile.getGame();
+        String cacheFileName = "%s.dxvk-cache".formatted(game.getCacheFileName());
 
-        response.setContentType("application/octet-stream");
-        response.setHeader(
-                "Content-Disposition",
-                "attachment;filename=%s.dxvk-cache".formatted(cacheFileName)
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDisposition(
+                ContentDisposition.attachment()
+                        .filename(cacheFileName)
+                        .build()
         );
 
-        StreamingResponseBody streamingResponseBody = outputStream -> {
-            InputStream cacheFileBinaryStreamFromDb;
-            try {
-                cacheFileBinaryStreamFromDb = cacheFile.getData().getBinaryStream();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+        InputStreamResource inputStreamResource = new InputStreamResource(cacheFileBlob.getBinaryStream());
 
-            cacheFileBinaryStreamFromDb.transferTo(outputStream);
-            outputStream.close();
-            cacheFileBinaryStreamFromDb.close();
-        };
-
-        return ResponseEntity.ok(streamingResponseBody);
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(cacheFileBlob.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(inputStreamResource);
     }
 
     @PostMapping
