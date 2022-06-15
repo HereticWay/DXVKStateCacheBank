930c7bce6d94821755747171eacc50efb22ed2e8
HereticWay <vpleft@gmail.com>
2022-06-15 17:09:15 +0200

    add GET endpoint to get the profile picture of the user;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/UserController.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/UserController.java
@@ -5,13 +5,17 @@ import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.UserCreateDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.UserInfoDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper.UserMapper;
 import com.dxvkstatecachebank.dxvkstatecachebank.service.UserService;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -33,6 +37,19 @@ public class UserController {
     public UserInfoDto findUserById(@PathVariable("userId") Long userId) {
         User userFound = userService.findById(userId);
         return userMapper.toDto(userFound);
+    }
+
+    @GetMapping("/{userId}/profile_picture")
+    public ResponseEntity<Resource> getProfilePictureByUserId(@PathVariable("userId") Long userId) throws SQLException {
+        User userFound = userService.findById(userId);
+        Blob profilePictureBlob = userFound.getProfilePicture();
+
+        InputStreamResource inputStreamResource = new InputStreamResource(profilePictureBlob.getBinaryStream());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .contentLength(profilePictureBlob.length())
+                .body(inputStreamResource);
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
