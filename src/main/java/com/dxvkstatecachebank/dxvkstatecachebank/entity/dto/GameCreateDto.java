c49b97d71754253e97cbd26601c99f1d600bd76e
HereticWay <vpleft@gmail.com>
2022-06-23 16:23:50 +0200

    add validation annotations to GameCreateDto;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/dto/GameCreateDto.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/entity/dto/GameCreateDto.java
@@ -5,12 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class GameCreateDto {
+    @NotEmpty
     private String name;
+
+    @NotEmpty
     private String cacheFileName;
+
+    @Positive
     private Long steamId;
 }
