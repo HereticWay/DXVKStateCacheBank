6314d710c9ab79f559c8dec983b2ed9195cd83ee
HereticWay <vpleft@gmail.com>
2022-06-28 08:29:59 +0200

    implements endpoints to modify name, email and password of users;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/UserController.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/UserController.java
@@ -4,6 +4,7 @@ import com.dxvkstatecachebank.dxvkstatecachebank.entity.User;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.CacheFileInfoDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.UserCreateDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.UserInfoDto;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.validator.annotation.ExistingUserId;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper.CacheFileMapper;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper.UserMapper;
 import com.dxvkstatecachebank.dxvkstatecachebank.service.CacheFileService;
@@ -19,6 +20,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.sql.Blob;
 import java.sql.SQLException;
@@ -96,6 +100,45 @@ public class UserController {
 
         User userCreated = userService.save(userMapper.toUser(userCreateDto, multipartFile));
         return ResponseEntity.ok(userMapper.toDto(userCreated));
+    }
+
+    @PutMapping("/{userId}/name")
+    public ResponseEntity<UserInfoDto> renameUser(@Valid @PathVariable("userId") @ExistingUserId Long userId, @Valid @RequestBody @NotEmpty String newName, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            log.error(bindingResult.getAllErrors().toString());
+            return ResponseEntity.badRequest().build();
+        }
+
+        User user = userService.findById(userId)
+                .orElseThrow();
+        user.setName(newName);
+        return ResponseEntity.ok(userMapper.toDto(userService.save(user)));
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<UserInfoDto> changePasswordOfUser(@Valid @PathVariable("userId") @ExistingUserId Long userId, @Valid @RequestBody @NotEmpty String newPassword, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            log.error(bindingResult.getAllErrors().toString());
+            return ResponseEntity.badRequest().build();
+        }
+
+        User user = userService.findById(userId)
+                .orElseThrow();
+        user.setPassword(newPassword);
+        return ResponseEntity.ok(userMapper.toDto(userService.save(user)));
+    }
+
+    @PutMapping("/{userId}/email")
+    public ResponseEntity<UserInfoDto> changeEmailOfUser(@Valid @PathVariable("userId") @ExistingUserId Long userId, @Valid @RequestBody @NotNull @Email String newEmail, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            log.error(bindingResult.getAllErrors().toString());
+            return ResponseEntity.badRequest().build();
+        }
+
+        User user = userService.findById(userId)
+                .orElseThrow();
+        user.setEmail(newEmail);
+        return ResponseEntity.ok(userMapper.toDto(userService.save(user)));
     }
 
     @DeleteMapping("/{userId}")
