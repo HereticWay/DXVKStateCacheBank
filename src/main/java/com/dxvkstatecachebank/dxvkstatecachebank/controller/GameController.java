423ba88fa5703b384a7b3cf7342c403ee3cd633b
HereticWay <vpleft@gmail.com>
2022-07-01 21:28:29 +0200

    fix GameController::getLatestIncrementalCacheFile;

--- src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/GameController.java
+++ src/main/java/com/dxvkstatecachebank/dxvkstatecachebank/controller/GameController.java
@@ -5,31 +5,27 @@ import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.CacheFileInfoDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.GameCreateDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.GameInfoDto;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.GameUpdateDto;
-import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.validator.annotation.ExistingGameId;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper.CacheFileMapper;
 import com.dxvkstatecachebank.dxvkstatecachebank.entity.mapper.GameMapper;
 import com.dxvkstatecachebank.dxvkstatecachebank.service.CacheFileService;
 import com.dxvkstatecachebank.dxvkstatecachebank.service.GameService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Positive;
+import java.io.IOException;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.time.ZoneOffset;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/game")
@@ -67,45 +63,37 @@ public class GameController {
         return ResponseEntity.ok(gameMapper.toDto(game));
     }
 
+    @Transactional
     @GetMapping("/{gameId}/incremental_cache_file")
-    public ResponseEntity<Resource> getLatestIncrementalCacheFile(@PathVariable("gameId") Long gameId) {
+    public void getLatestIncrementalCacheFile(@PathVariable("gameId") Long gameId, HttpServletResponse response) {
         if (!gameService.existsById(gameId)) {
             log.error("Game id: {} could not be found", gameId);
-            return ResponseEntity.notFound().build();
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
         }
 
         Game game = gameService.findById(gameId)
                 .orElseThrow();
         Blob cacheFileBlob = game.getIncrementalCacheFile();
-        InputStreamResource cacheFileStreamResource;
-        long cacheFileLength;
         try {
-            if(cacheFileBlob == null || (cacheFileLength = cacheFileBlob.length()) == 0) {
+            if(cacheFileBlob == null || cacheFileBlob.length() == 0) {
                 log.error("There's no incremental cache for the following game id {}", gameId);
-                return ResponseEntity.notFound().build();
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                return;
             }
-            cacheFileStreamResource = new InputStreamResource(cacheFileBlob.getBinaryStream());
-        } catch (SQLException e) {
+
+            String cacheFileName = "%s.dxvk-cache".formatted(game.getCacheFileName());
+
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + cacheFileName + "\"");
+            response.setContentLengthLong(cacheFileBlob.length());
+            response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+            response.setHeader(HttpHeaders.LAST_MODIFIED, game.getIncrementalCacheLastModified().toInstant(ZoneOffset.UTC).toString());
+            IOUtils.copyLarge(cacheFileBlob.getBinaryStream(), response.getOutputStream());
+        } catch (SQLException | IOException e) {
             log.error("Some unexpected error occurred!");
             e.printStackTrace();
-            return ResponseEntity.internalServerError().build();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-
-        String cacheFileName = "%s.dxvk-cache".formatted(game.getCacheFileName());
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentDisposition(
-                ContentDisposition.attachment()
-                        .filename(cacheFileName)
-                        .build()
-        );
-
-        return ResponseEntity.ok()
-                .headers(headers)
-                .contentLength(cacheFileLength)
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .lastModified(game.getIncrementalCacheLastModified().toInstant(ZoneOffset.UTC))
-                .body(cacheFileStreamResource);
     }
 
     @GetMapping("/{gameId}/cache_files")
