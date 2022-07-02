ef46e4ff95a43f779235c6d8ea9c24663c0cc181
HereticWay <vpleft@gmail.com>
2022-07-02 21:18:53 +0200

    add more tests to GameControllerIntegrationTest;

--- src/test/java/com/dxvkstatecachebank/dxvkstatecachebank/integrationtest/GameControllerIntegrationTest.java
+++ src/test/java/com/dxvkstatecachebank/dxvkstatecachebank/integrationtest/GameControllerIntegrationTest.java
@@ -1,8 +1,6 @@
 package com.dxvkstatecachebank.dxvkstatecachebank.integrationtest;
 
-import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.CacheFileUploadDto;
-import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.GameInfoDto;
-import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.UserInfoDto;
+import com.dxvkstatecachebank.dxvkstatecachebank.entity.dto.*;
 import com.dxvkstatecachebank.dxvkstatecachebank.service.CacheFileService;
 import com.dxvkstatecachebank.dxvkstatecachebank.service.GameService;
 import com.dxvkstatecachebank.dxvkstatecachebank.service.UserService;
@@ -78,6 +76,18 @@ public class GameControllerIntegrationTest {
     }
 
     @Test
+    void emptyDatabase_addNewGameWithInvalidData_shouldReturn422UnprocessableEntity() {
+        ResponseEntity<GameInfoDto> creationResponse = requestUtils.postGame(
+                GameCreateDto.builder()
+                        .name(null)
+                        .cacheFileName(null)
+                        .steamId(543L)
+                        .build()
+        );
+        assertThat(creationResponse.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @Test
     void emptyDatabase_addNewGame_getGameByItsId_shouldReturnAddedGame() {
         ResponseEntity<GameInfoDto> creationResponse = requestUtils.postGame(SAMPLE_GAME_CREATE_DTO_APEX);
         assertThat(creationResponse.getStatusCode())
@@ -143,5 +153,97 @@ public class GameControllerIntegrationTest {
         assertThat(fileStreamSize.getResponseStatus()).isEqualTo(HttpStatus.OK);
         assertThat(fileStreamSize.getContentLengthHeader()).isEqualTo(correctContentLength);
         assertThat(fileStreamSize.getRealFileLength()).isEqualTo(correctContentLength);
+    }
+
+    @Test
+    void noGamesInDatabase_getCacheFilesByRandomGameId_shouldReturn404NotFound() {
+        long randomGameId = 6753L;
+        ResponseEntity<CacheFileInfoDto[]> response = requestUtils.getAllCacheFilesByGameId(randomGameId);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void oneGameInDatabase_noCacheFilesInDatabase_getCacheFilesByGameId_shouldReturnEmptyArray() {
+        ResponseEntity<GameInfoDto> gameCreationResponse = requestUtils.postGame(SAMPLE_GAME_CREATE_DTO_APEX);
+        assertThat(gameCreationResponse.getBody()).isNotNull();
+        long gameId = gameCreationResponse.getBody()
+                .getId();
+
+        ResponseEntity<CacheFileInfoDto[]> response = requestUtils.getAllCacheFilesByGameId(gameId);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody())
+                .isNotNull()
+                .isEmpty();
+    }
+
+    @Test
+    void emptyDatabase_updateGameBySomeRandomId_shouldReturn404NotFound() {
+        GameUpdateDto gameUpdateDto = GameUpdateDto.builder()
+                .name("Resident Evil 5")
+                .cacheFileName("re5")
+                .steamId(543L)
+                .build();
+        long randomGameId = 342L;
+        ResponseEntity<GameInfoDto> updateResponse = requestUtils.updateGame(randomGameId, gameUpdateDto);
+        assertThat(updateResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void oneGameInDatabase_updateGameWithInvalidData_shouldReturn422UnprocessableEntity() {
+        ResponseEntity<GameInfoDto> gameCreationResponse = requestUtils.postGame(SAMPLE_GAME_CREATE_DTO_APEX);
+        assertThat(gameCreationResponse.getBody()).isNotNull();
+        long gameId = gameCreationResponse.getBody()
+                .getId();
+
+        GameUpdateDto gameUpdateDto = GameUpdateDto.builder()
+                .name(null)
+                .cacheFileName("re5")
+                .steamId(null)
+                .build();
+        ResponseEntity<GameInfoDto> updateResponse = requestUtils.updateGame(gameId, gameUpdateDto);
+        assertThat(updateResponse.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @Test
+    void oneGameInDatabase_updateGame_getGameShouldReturnUpdatedGame() {
+        ResponseEntity<GameInfoDto> gameCreationResponse = requestUtils.postGame(SAMPLE_GAME_CREATE_DTO_APEX);
+        assertThat(gameCreationResponse.getBody()).isNotNull();
+        long gameId = gameCreationResponse.getBody()
+                .getId();
+
+        GameUpdateDto gameUpdateDto = GameUpdateDto.builder()
+                .name("Resident Evil 5")
+                .cacheFileName("re5")
+                .steamId(null)
+                .build();
+        ResponseEntity<GameInfoDto> updateResponse = requestUtils.updateGame(gameId, gameUpdateDto);
+        assertThat(updateResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        ResponseEntity<GameInfoDto> response = requestUtils.getGameById(gameId);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull()
+                .isEqualTo(updateResponse.getBody());
+        assertThat(response.getBody().getName()).isEqualTo(gameUpdateDto.getName());
+        assertThat(response.getBody().getCacheFileName()).isEqualTo(gameUpdateDto.getCacheFileName());
+        assertThat(response.getBody().getSteamId()).isEqualTo(gameUpdateDto.getSteamId());
+    }
+
+    @Test
+    void emptyDatabase_deleteGameByRandomId_shouldReturn404NotFound() {
+        long randomGameId = 43278L;
+        ResponseEntity<GameInfoDto> response = requestUtils.deleteGame(randomGameId);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void oneGameInDatabase_deleteGameById_getGame_shouldReturn404Notfound() {
+        ResponseEntity<GameInfoDto> gameCreationResponse = requestUtils.postGame(SAMPLE_GAME_CREATE_DTO_APEX);
+        assertThat(gameCreationResponse.getBody()).isNotNull();
+        long gameId = gameCreationResponse.getBody()
+                .getId();
+
+        requestUtils.deleteGame(gameId);
+        ResponseEntity<GameInfoDto> response = requestUtils.getGameById(gameId);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
